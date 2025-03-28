4254f88d53020676d353c772d3e8921c3ef21331
kat <kat@postefix.de>
2025-03-28 15:23:30 +0000

    feat: add tests and css

---
+++ backend/frontend/healthifyme-frontend/src/components/CookiePolicy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CookiePolicy = () => (
+  <div>
+    <h1>Cookie Policy</h1>
+    <p className='cookie-policy'>Our website uses cookies that are necessary for the operation of the website. These cookies do not collect personal data, and they are essential to keep the website functioning properly.</p>
+    <p className='cookie-policy'>If you have any questions regarding our cookie usage, feel free to contact us at [your email].</p>
+  </div>
+);
+
+export default CookiePolicy;
